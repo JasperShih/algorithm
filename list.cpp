1f9a1b1d57445997e22cb6a5cb022e92931f2492
shih <shih@localhost.localdomain>
2017-09-07 23:58:59 +0800

    Modify list.cpp(version 2)  On branch master 	modified:   list.cpp

--- list.cpp
+++ list.cpp
@@ -3,15 +3,21 @@
 //
 #include <iostream>
 #include <string.h>
-#include <zconf.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 using namespace std;
 
 typedef struct List {
+    bool tail;
     void *data;
     List *next;
 } List;
+
+//get_tail: List* -> bool
+bool get_tail(List *list) {
+    return list->tail;
+}
 
 //get_data: List* -> void*
 void *get_data(List *list) {
@@ -20,11 +26,21 @@ void *get_data(List *list) {
 
 //get_next: List* -> List*
 List *get_next(List *list) {
-    return list->next;
+    if (get_tail(list)) {
+        throw exception();
+    } else {
+        return list->next;
+    }
+}
+
+//set_tail: bool X List* -> ()
+void set_tail(bool is_tail, List *list) {
+    list->tail = is_tail;
 }
 
 //set_data: void* X size_t X List* -> ()
 void set_data(void *data, size_t size, List *list) {
+    free(get_data(list));
     list->data = calloc(size, 1);
     memcpy(get_data(list), data, size);
 }
@@ -32,76 +48,48 @@ void set_data(void *data, size_t size, List *list) {
 //set_next: List* X List* -> ()
 void set_next(List *next, List *list) {
     list->next = next;
+    set_tail(false, list);
 }
 
 //------------------------------------------------
 
-//create_list: () -> List*
-List *create_list() {
+//trans_to_list: void* X size_t -> List*
+List *trans_to_list(void *data, size_t size) {
     List *list = (List *) calloc(1, sizeof(List));
-    bzero(list, sizeof(List));
+    set_data(data, size, list);
+    set_tail(true, list);
     return list;
 }
 
-//is_tail: List* -> bool
-bool is_tail(List *list) {
-    return get_data(list) == NULL &&
-           get_next(list) == NULL;
-}
-
-//del_list: List* -> ()
-void del_list(List *list) {
-    List *tmp;
-
-    if (is_tail(list)) {
-        free(list);
-        return;
-    } else {
-        tmp = get_next(list);
-        free(get_data(list));
-        free(list);
-        return del_list(tmp);
-    }
+//add_elem: void* X size_t X List* -> List*
+List *add_elem(void *data, size_t size, List *list) {
+    List *elem = trans_to_list(data, size);
+    set_next(list, elem);
+    return elem;
 }
 
 //add_list: List* X List* -> List*
-List *add_list(List *pre_list, List *post_list) {
-    List *pre_now = NULL;
-    List *now     = pre_list;
+List *add_list(List *pre, List *post) {
+    List *now = pre;
 
     for (;;) {
-        if (is_tail(now)) {
-            del_list(now);
-            if (pre_now == NULL) {
-                return post_list;
-            } else {
-                set_next(post_list, pre_now);
-                return pre_list;
-            }
+        if (get_tail(now)) {
+            set_next(post, now);
+            return pre;
         } else {
-            pre_now = now;
-            now     = get_next(now);
+            now = get_next(now);
         }
     }
-}
-
-//===================================================
-
-//add_elem: void* X size_t X List* -> List*
-List *add_elem(void *data, size_t size, List *list) {
-    List *elem = create_list();
-
-    set_data(data, size, elem);
-    elem->next = list;
-    return elem;
 }
 
 //del_elem: List* -> List*
 List *del_elem(List *list) {
     List *tmp;
 
-    if (is_tail(list)) {
-        throw exception();
+    if (get_tail(list)) {
+        free(get_data(list));
+        free(list);
+        return NULL;
     } else {
         tmp = get_next(list);
         free(get_data(list));
@@ -110,51 +98,33 @@ List *del_elem(List *list) {
     }
 }
 
-void *iter_list(bool (*function_ptr1)(List *list),
-                void *(*function_ptr2)(List *list),
-                List *list) {
-    if (is_tail(list)) {
-        return function_ptr2(list);
+//del_list: List* -> ()
+void del_list(List *list) {
+    list = del_elem(list);
+    if (list == NULL) {
+        return;
     } else {
-        if (function_ptr1(list)) {
-            return list;
-        } else {
-            return iter_list(function_ptr1, function_ptr2, get_next(list));
-        }
+        return del_list(list);
     }
 }
 
-bool display(List *list) {
-    cout << *(int *) get_data(list) << endl;
-    return false;
+void display(List *list) {
+    if (get_tail(list)) {
+        cout << *(int *) get_data(list) << endl;
+        return;
+    } else {
+        cout << *(int *) get_data(list) << endl;
+        return display(get_next(list));
+    }
 }
-
-void *noooo(List *list) {
-
-}
-
-bool find2(void *data) {
-    return *(int *) data == 2;
-}
-
-//void display(List *list) {
-//    if (is_tail(list)) {
-//        return;
-//    } else {
-//        cout << *(int *) get_data(list) << endl;
-//        return display(get_next(list));
-//    }
-//}
 
 int main() {
-    int  num1   = 1;
-    int  num2   = 2;
-    int  num3   = 3;
-    List *list  = create_list();
-    List *list2 = create_list();
-    List *tmp;
+    int  num1  = 1;
+    int  num2  = 2;
+    int  num3  = 3;
+    List *list = trans_to_list(&num1, sizeof(num1));
+    List *list2 = trans_to_list(&num1, sizeof(num1));
 
-    list = add_elem(&num1, sizeof(num1), list);
     list = add_elem(&num2, sizeof(num2), list);
     list = add_elem(&num3, sizeof(num3), list);
 
@@ -167,8 +137,7 @@ int main() {
 
     list = add_list(list, list2);
 
-    iter_list(display, noooo, list);
-//    tmp = (List *) iter_list(find2, list);
+    display(list);
 
     del_list(list);
 }
